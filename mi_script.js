bcc80daaabb12bfbb2308d22b81769140bc2f3ab
figueredo-870 <140459285+figueredo-870@users.noreply.github.com>
2023-08-02 20:09:16 -0300

    Add files via upload

---
+++ mi_script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", el número de enlaces de la página :
+    const enlaces = document.getElementsByTagName('a');
+    const numeroEnlaces = enlaces.length;
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", la dirección del penúltimo enlace de la página :
+
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", el número de enlaces que apuntan a http://prueba/ :
+
+    // Incluye aquí el código necesario para mostrar en el párrafo "info", el número de enlaces del tercer párrafo :
+});
